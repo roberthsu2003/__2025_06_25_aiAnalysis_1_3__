31f3940453eb76f30e5dc9f70016033cca891ca2
徐國堂 <roberthsu2003@gmail.com>
2025-07-23 13:50:35 +0000

    modify

--- lesson9/index.py
+++ lesson9/index.py
@@ -1,17 +1,16 @@
-import tools
-from tools import play_game,Empty
+import box
 
 def main():
     try:
         play_count = 0
         while(True):
             play_count += 1
-            play_game()
+            box.play_game()
             is_continue = input("您還要繼續嗎(y,n)?")
             if is_continue == "n":
                 break
             
-        print(f"{tools.user_name}共玩了{play_count}次")
+        print(f"{box.user_name}共玩了{play_count}次")
         print("遊戲結束")
     except ValueError:
         print("格式錯誤")
