c1977538c9f626427c48b04fd3c169aaecf7b48d
徐國堂 <roberthsu2003@gmail.com>
2025-09-08 20:14:02 +0800

    modify

--- lesson21/app.py
+++ lesson21/app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request
 from google import genai
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
 
 
 app = Flask(__name__)
 
 # 初始化 Gemini 客戶端
-client = genai.Client()
+client = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
 
 # 首頁路由
 @app.route('/')
